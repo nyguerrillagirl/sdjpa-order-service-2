7333cd6aa2cabc47401784297757e06d10a4d2b1
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-12-19 10:22:45 -0500

    Initial Project

---
+++ src/test/java/com/example/sdjpaorderservice2/SdjpaOrderService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdjpaorderservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaOrderService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
